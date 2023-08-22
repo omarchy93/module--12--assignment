41aa28ddd0ec73290447556d7ecf3f1925e7d4de
omar <omarfarukchy93@gmail.com>
2023-08-22 23:41:55 +0600

    add error for error data

---
+++ src/app/blog/error.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="m-16 p-16 text-center text-6xl font-bold bg-gradient-to-r from-red-700 to-red-300 bg-clip-text text-transparent">
+          Blog details data not Found
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
