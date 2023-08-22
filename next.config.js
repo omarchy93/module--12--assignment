6b1f8cb7f17f9ed77080654146645fbc25d0ce20
omar <omarfarukchy93@gmail.com>
2023-08-23 00:35:22 +0600

    next.config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/:path*",
-  //       headers: [
-  //         {
-  //           key: "X-Frame-Options",
-  //           value: "DENY",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
-  // distDir: "build",
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
+  distDir: "build",
   images: {
     domains: ["basic-blog.teamrabbil.com", "roar.media"],
   },
